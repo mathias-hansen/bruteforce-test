981fcf0357b760b50fa4ff7b18a44bdf171780b0
Mathias hansen <mathias.hansen783@gmail.com>
2015-03-16 08:42:55 +0100

    Fixed stuff

--- Program.cs
+++ Program.cs
@@ -5,64 +5,57 @@ using System.Linq;
 using System.Net;
 using System.Runtime.InteropServices;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace bruteforcetest
 {
     class Program
     {
+        public static List<char> Chars = GetChars();
+        public static string Password = string.Empty;
+        public static int Count = 0;
 
         static void Main(string[] args)
         {
-            var t = ItteratePasswords("", 0);
+            ItteratePasswords(8);
+
+            if (Password != "") Console.WriteLine(">> " + Password + " <<");
         }
 
-        public static string ItteratePasswords(string prefix, int depth)
-        {
-            int MaxLength = 2;
-            string password;
-            string result;
-            int i = 0;
 
-            foreach (var c in Chars)
+        public static void ItteratePasswords(int depth, string prefix = "")
+        {
+            Parallel.ForEach(Chars, (c) =>
             {
-                password = GetNextPassword(i, prefix);
-                if (password != null)
+                if (Password != "") return;
+
+                var password = prefix + c;
+                var response = SendRequest(password);
+
+                Console.WriteLine(password + " - " + Count);
+
+                if (response != "false" && response != null)
                 {
-                    if (SendRequest(password) != "false") return password;
-                    if ((depth + 1) < MaxLength)
-                    {
-                        result = ItteratePasswords(password, (depth + 1));
-                        if (result != "false" && result != null) return result;
-                    }
+                    Password = password;
+                    return;
                 }
-                i++;
-            }
-            return null;
+                else
+                {
+                    if (depth - 1 > 0) ItteratePasswords(depth - 1, password);
+                    if (Password != "") return;
+                }
+                Count++;
+            });
         }
-
-        public static string GetNextPassword(int i, string prefix)
-        {
-            try
-            {
-                return prefix + Chars[(i + 1)];
-            }
-            catch (Exception)
-            {
-                return null;
-            }
-            
-            
-        }
-
-        public static List<char> Chars = GetChars();
+        
         public static List<char> GetChars()
         {
             var chars = new List<char>();
             for (int i = char.MinValue; i <= 126; i++)
             {
                 char c = Convert.ToChar(i);
-                if (!char.IsControl(c))
+                if (Regex.Match(c.ToString(), "[cws52qbz]").Success)
                 {
                     chars.Add(c);
                 }
